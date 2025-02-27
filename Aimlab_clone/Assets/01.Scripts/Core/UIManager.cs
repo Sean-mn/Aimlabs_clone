56f521f533dfec6634afa9709212e38f0cd65dd0
Sean-mn <b01080080329@gmail.com>
2025-02-27 14:33:44 +0900

    UIManager 스크립트 수정

--- Aimlab_clone/Assets/01.Scripts/Core/UIManager.cs
+++ Aimlab_clone/Assets/01.Scripts/Core/UIManager.cs
@@ -1,17 +1,9 @@
 using UnityEngine;
-using UnityEngine.UI;
 
-public class UIManager : MonoBehaviour
+public class UIManager : MonoSingleton<UIManager>
 {
-    [SerializeField] Text timer;
-
-    private void Update()
+    public void ActiveUI<T>(T ui) where T : Component
     {
-        TimerText();
-    }
-
-    private void TimerText()
-    {
-        timer.text = string.Format("{0:D2}:{1:D2}", GameManager.Instance.min, (int)GameManager.Instance.sec);
+        ui?.gameObject?.SetActive(true);
     }
 }
