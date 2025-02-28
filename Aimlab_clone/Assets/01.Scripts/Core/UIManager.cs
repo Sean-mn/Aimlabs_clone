a9242a1e8d6970f8faf24645b179eea1cdd41a75
Sean-mn <b01080080329@gmail.com>
2025-02-28 10:36:33 +0900

    UIManager 수정

--- Aimlab_clone/Assets/01.Scripts/Core/UIManager.cs
+++ Aimlab_clone/Assets/01.Scripts/Core/UIManager.cs
@@ -2,8 +2,29 @@ using UnityEngine;
 
 public class UIManager : MonoSingleton<UIManager>
 {
+    [field: SerializeField, Header("Countdown UI")]
+    public UI_Countdown CountdownUI { get; private set; }
+
+    protected override void Awake()
+    {
+        Init();
+
+        base.Awake();
+    }
+
+    private void Init()
+    {
+        if (CountdownUI == null)
+            CountdownUI = FindObjectOfType<UI_Countdown>();
+    }
+
     public void ActiveUI<T>(T ui) where T : Component
     {
         ui?.gameObject?.SetActive(true);
+    }
+
+    public void DisableUI<T> (T ui) where T : Component
+    {
+        ui?.gameObject?.SetActive(false);
     }
 }
