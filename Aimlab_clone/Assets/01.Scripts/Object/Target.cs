50123de8b2fd09d9213482beb94d1e0bf2607590
Sean-mn <b01080080329@gmail.com>
2025-02-28 10:37:46 +0900

    Tartget 스크립트 수정

--- Aimlab_clone/Assets/01.Scripts/Object/Target.cs
+++ Aimlab_clone/Assets/01.Scripts/Object/Target.cs
@@ -1,46 +1,6 @@
 using UnityEngine;
-using UnityEngine.Pool;
     
 public class Target : MonoBehaviour
 {
     [SerializeField] GameObject targetPrefab;
-    ObjectPool<GameObject> targetPool;
-
-    private void Start()
-    {
-        targetPool = new ObjectPool<GameObject>(
-            CreateTarget,
-            OnGetObject,
-            OnReleaseObject,
-            OnDestroyObject,
-            collectionCheck: true,
-            defaultCapacity: 6,
-            maxSize: 15
-            );
-    }
-
-    private GameObject CreateTarget()
-    {
-        return Instantiate(targetPrefab);
-    }
-
-    private void OnGetObject(GameObject obj)
-    {
-        obj.SetActive(true);
-    }
-
-    private void OnReleaseObject(GameObject obj)
-    {
-        obj.SetActive(false);
-    }
-
-    private void OnDestroyObject(GameObject obj)
-    {
-        Destroy(obj);
-    }
-
-    public void ReturnObject()
-    {
-
-    }
 }
