4fd00f4432d5ca6d044752f672b1e3d6420a158f
Sean-mn <b01080080329@gmail.com>
2025-02-28 10:37:36 +0900

    UI_Timer 스크립트 구현

--- Aimlab_clone/Assets/01.Scripts/UI/UI_Timer.cs
+++ Aimlab_clone/Assets/01.Scripts/UI/UI_Timer.cs
@@ -4,17 +4,26 @@ using UnityEngine.UI;
 
 public class UI_Timer : UI
 {
-    public Text TimerText {  get; private set; }
+    [field: SerializeField, Header("Timer UI")]
+    public Text TimerText { get; private set; }
 
     protected override void Init()
     {
         TimerText = GetComponent<Text>();
     }
 
+    private void Update()
+    {
+        if (GameManager.Instance.IsGameOver)
+            UIManager.Instance.DisableUI<Text>(TimerText);
+
+        UIFunction();
+    }
+
     public override void UIFunction()
     {
-        int min = Mathf.FloorToInt(GameManager.Instance.Time / 60);
-        int sec = Mathf.FloorToInt(GameManager.Instance.Time % 60);
+        int min = Mathf.FloorToInt(GameManager.Instance.GameTime / 60);
+        int sec = Mathf.FloorToInt(GameManager.Instance.GameTime % 60);
 
         TimerText.text = string.Format("{0:00}:{1:00}", min, sec);
     }
