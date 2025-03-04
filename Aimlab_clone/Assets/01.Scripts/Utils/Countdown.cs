a16c75f23a2520963a2a1726c1b9c1fd11e6b854
Sean-mn <b01080080329@gmail.com>
2025-03-04 16:19:04 +0900

    Countdown 스크립트 변경사항
    UIManager 스크립트의 CountdownUI의 Null 체크

--- Aimlab_clone/Assets/01.Scripts/Utils/Countdown.cs
+++ Aimlab_clone/Assets/01.Scripts/Utils/Countdown.cs
@@ -6,19 +6,26 @@ public class Countdown
 {
     public static IEnumerator StartCountdown(Action call)
     {
+        if (UIManager.Instance?.CountdownUI == null)
+        {
+            Debug.LogWarning("CountdownUI = null");
+            call?.Invoke();
+            yield break;
+        }
+
         WaitForSeconds delay = new WaitForSeconds(1);
 
         for (int i = 3; i > 0; i--)
         {
-            UIManager.Instance.CountdownUI?.UIFunction(i);
+            UIManager.Instance.CountdownUI.UIFunction(i);
             yield return delay;
         }
 
         UIManager.Instance.CountdownUI.CountdownText.text = "Start";
-
         yield return delay;
 
-        UIManager.Instance.CountdownUI.CountdownText.gameObject.SetActive(false);
         call?.Invoke();
+
+        UIManager.Instance.CountdownUI.CountdownText.gameObject.SetActive(false);
     }
 }
