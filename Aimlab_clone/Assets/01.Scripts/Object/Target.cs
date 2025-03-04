dccbbb7d95ee6ed6860ef5ef4521a1bd26523ba1
Sean-mn <b01080080329@gmail.com>
2025-03-04 17:51:18 +0900

    오브젝트 스크립트 변경

--- Aimlab_clone/Assets/01.Scripts/Object/Target.cs
+++ Aimlab_clone/Assets/01.Scripts/Object/Target.cs
@@ -3,4 +3,14 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     [SerializeField] GameObject targetPrefab;
+
+    private void Awake()
+    {
+        targetPrefab = GetComponent<GameObject>();
+    }
+
+    private void OnDisable()
+    {
+        ScoreManager.Instance.AddScore(100);
+    }
 }
