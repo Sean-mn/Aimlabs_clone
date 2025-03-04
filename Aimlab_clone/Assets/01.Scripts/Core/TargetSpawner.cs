6f1abaad6720dd626a66b521c4716732c755d44c
Sean-mn <b01080080329@gmail.com>
2025-03-04 17:50:23 +0900

    TargetSpawner 스크립트 구현

---
+++ Aimlab_clone/Assets/01.Scripts/Core/TargetSpawner.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject targetPrefab;
+
+    [SerializeField]
+    private List<Transform> targetPoses = new List<Transform>();
+
+    private void OnEnable()
+    {
+        GameManager.Instance.onGameStart -= FirstCreateTargets;
+        GameManager.Instance.onGameStart += FirstCreateTargets;
+    }
+
+    private void OnDisable()
+    {
+        GameManager.Instance.onGameStart -= FirstCreateTargets;
+    }
+    private void FirstCreateTargets()
+    {
+        if (targetPoses.Count < 7) return;
+
+        List<Transform> mixedPoses = new List<Transform>(targetPoses);
+        for (int i = mixedPoses.Count - 1; i > 0; i--)
+        {
+            int randomIndex = Random.Range(0, i + 1);
+            (mixedPoses[i], mixedPoses[randomIndex]) = (mixedPoses[randomIndex], mixedPoses[i]);
+        }
+
+        for (int i = 0; i < 7; i++)
+        {
+            Instantiate(targetPrefab, mixedPoses[i].position, Quaternion.identity);
+        }
+    }
+
+    public IEnumerator Spawn()
+    {
+        WaitForSeconds delay = new WaitForSeconds(0.2f);
+
+        while (GameManager.Instance.GameTime < GameManager.Instance.MaxGameTime)
+        {
+            yield return delay;
+
+            int createPos = Random.Range(0, targetPoses.Count);
+
+            Instantiate(targetPrefab, targetPoses[createPos].position, Quaternion.identity);
+        }
+    }
+}
