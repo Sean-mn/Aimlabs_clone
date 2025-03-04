eafa6a02d1d81cf5e0b878aff52284b221fe8103
Sean-mn <b01080080329@gmail.com>
2025-03-04 19:47:20 +0900

    UI_ShowScore 스크립트 생성

---
+++ Aimlab_clone/Assets/01.Scripts/UI/UI_ShowScore.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_ShowScore : UI
+{
+    [field: SerializeField, Header("Score Text")]
+    public Text ScoreText { get; private set; }
+
+    protected override void Init()
+    {
+        ScoreText= GetComponent<Text>();
+    }
+
+
+}
