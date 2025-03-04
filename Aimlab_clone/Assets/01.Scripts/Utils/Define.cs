3dddf6470891e39a253899d4b58ee8e58ab80699
Sean-mn <b01080080329@gmail.com>
2025-03-04 20:46:46 +0900

    Define 스크립트 구현

---
+++ Aimlab_clone/Assets/01.Scripts/Utils/Define.cs
@@ -0,0 +1,15 @@
+public class Define
+{
+    public class Tags
+    {
+        public const string Player = "Player";
+        public const string Target = "Target";
+    }
+
+    public enum Layers
+    {
+        Ground,
+        Player,
+        Target,
+    }
+}
