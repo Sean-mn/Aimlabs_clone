96b38852b1d41d8d67299e4b79069f5dda15a602
Sean-mn <b01080080329@gmail.com>
2025-03-04 21:15:07 +0900

    PlayScene 스크립트 수정
    Clear 매서드 GameManager 스크립트의 onGameOver 이벤트 등록

--- Aimlab_clone/Assets/01.Scripts/Scenes/PlayScene.cs
+++ Aimlab_clone/Assets/01.Scripts/Scenes/PlayScene.cs
@@ -5,6 +5,17 @@ public class PlayScene : BaseScene
         Init();
     }
 
+    private void OnEnable()
+    {
+        GameManager.Instance.onGameOver -= Clear;
+        GameManager.Instance.onGameOver += Clear;
+    }
+
+    private void OnDestroy()
+    {
+        GameManager.Instance.onGameOver -= Clear;
+    }
+
     protected override void Init()
     {
         SceneType = Define.Scene.Play;
